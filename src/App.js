f9a1adbe5cf44057b618df7a49a3de5c2e72c2a2
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-06-14 19:15:45 +0000

    error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
 import Timer from "./Component/Timer";
+import ErrorBoundary from "./Component/ErrorBoundary";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      timerOn: false,
+    };
+  }
 
-function App() {
-  return <Timer />;
+  handleTimerOn = () => {
+    this.setState((prevState) => ({
+      timerOn: !prevState.timerOn,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+      <ErrorBoundary>
+            <Timer timerOn={this.state.timerOn} />
+        </ErrorBoundary>
+      
+    
+        <div className="button-container">
+          <button onClick={this.handleTimerOn} className={this.state.timerOn ? 'active' : 'button'}>
+            {this.state.timerOn ? 'STOP' : 'START'}
+          </button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/Component/Timer.js
+++ src/Component/Timer.js
@@ -6,16 +6,20 @@ class Timer extends React.Component {
   constructor() {
     super();
     this.state = {
-      count: 0,
+    //   count: 0,
+      time: 0,
     };
+    this.timer = null;
     console.log("Constructor");
   }
-  handleClick = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-    console.log(this.state.count);
-  };
+  // handleClick = () => {
+  //   const {count} = this.state;
+  //   this.setState({
+  //     count: count+1
+  //   });
+  //   console.log(count);
+  // };
+
   //this belongs to Mounting Phase
   static getDerivedStateFromProps() {
     console.log("getDerivedStateFromProps");
@@ -24,19 +28,19 @@ class Timer extends React.Component {
 
   //this belongs to Updating Phase
   shouldComponentUpdate(nextProps, nextState) {
-    return true;
-  }
+return true}
   //this belongs to Updating Phase
 
   render() {
-    const { count } = this.state;
+    // const { count } = this.state;
     console.log("render");
     return (
       <>
-        <h1 className="h1">Timer Rendered</h1>
-        <button onClick={this.handleClick}>Click Me</button> &nbsp; &nbsp;
+      
+        <h1 className="h1">STOPWATCH</h1>
         &nbsp;
-        <span>{count}</span>
+        {/* <span>{count}</span> */}
+        <h3>{new Date(this.state.time * 1000).toISOString().slice(11,19)}</h3>
       </>
     );
   }
@@ -47,8 +51,21 @@ class Timer extends React.Component {
     console.log("getSnapshotBeforeUpdate");
     return null;
   }
-  componentDidUpdate() {
+  componentDidUpdate(prevprops, prevstate, snapshot) {
     console.log("component Did Update");
-  }
+    if (prevprops.timerOn !== this.props.timerOn) {
+      if (this.props.timerOn) {
+        // Start the timer
+        this.timer = setInterval(() => {
+          this.setState((prevState) => ({
+            time: prevState.time + 1,
+          }));
+        }, 1000);
+      } else {
+        // Stop the timer
+        clearInterval(this.timer);
+      }
+}
+}
 }
 export default Timer;
